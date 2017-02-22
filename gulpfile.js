9c250d33a9fa27e591ddea25759b4b15479ffcaa
maehner <maehner@kontrollfeld>
2017-02-23 00:27:09 +0100

    Main Menu

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var $    = require('gulp-load-plugins')();
 
 var sassPaths = [
   'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
+  'bower_components/motion-ui/src',
 ];
 
 gulp.task('sass', function() {
